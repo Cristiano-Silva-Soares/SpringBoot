fd0573ec126ebc14c1d8b793a98c5b6b1dca450d
Cristiano-Silva-Soares <criztianosoares99@outlook.com>
2021-08-23 16:48:13 -0300

    Lista de Spring

---
+++ habilities/src/main/java/com/habilidades/habilities/HabilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.habilidades.habilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabilitiesApplication.class, args);
+	}
+
+}
